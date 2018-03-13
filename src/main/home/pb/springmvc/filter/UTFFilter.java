2a4568cca4fd7449f4e28b1fd4490a2f8cebfee3
ChiSaiM <944125621@qq.com>
2018-03-13 18:21:43 +0800

    add commit

---
+++ src/main/home/pb/springmvc/filter/UTFFilter.java
@@ -0,0 +1,20 @@
+package home.pb.springmvc.filter;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class UTFFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("utf-8");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    public void destroy() {
+
+    }
+}
